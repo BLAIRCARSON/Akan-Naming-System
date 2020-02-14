861123c6acb07b6ed1f57b17baca7fb71d9bb02d
BA <engineervinceblair@gmail.com>
2020-02-14 14:42:32 +0300

    declared variables for akan names

--- script.js
+++ script.js
@@ -1,22 +1,52 @@
 var femaleAkanNames = [
-	"Akosua, <img src='https://bit.ly/2JsWQ5t' height='200px'>",
-	"Adwoa, <img src='https://bit.ly/369OsS7' height='200px'>",
-	"Abenaa, <img src='https://bit.ly/2MRihiN' height='200px'>",
-	"Akua, <img src='https://bit.ly/2Poskxu' height='200px'>",
-	"Yaa, <img src='https://bit.ly/2PoBN7T' height='200px'>",
-	"Afua, <img src='https://bit.ly/2pj47xU' height='200px'>",
-	"Ama, <img src='https://bit.ly/348GTcI' height='200px'>"
+	"Akosua",
+	"Adwoa",
+	"Abenaa",
+	"Akua",
+	"Yaa",
+	"Afua",
+	"Ama" 
 ];
 var maleAkanNames = [
-	"Kwasi, <img src='https://bit.ly/2omOfdc' height='200px'>",
-	"Kwadwo, <img src='https://bit.ly/31SWwmV' height='200px'>",
-	"Kwabena, <img src='https://bit.ly/2PphxDi' height='200px'>",
-	"Kwaku, <img src='https://bit.ly/3683NCG' height='200px'>",
-	"Yaw, <img src='https://bit.ly/36eVwx0' height='200px'>",
-	"Kofi, <img src='https://bit.ly/2JsXMH1' height='200px'>",
-	"Kwame, <img src='https://bit.ly/2BRxDNG' height='200px'>"
+	"Kwasi",
+	"Kwadwo",
+	"Kwabena",
+	"Kwaku",
+	"Yaw",
+	"Kofi",
+	"Kwame"
 ];
-var daysWeek=[
+var daysOfWeek=[
     "Sunday","Monday","Tuesday", "Wednesday", "Thursday","Friday","Saturday"
  ];
  
+
+ //calling functions
+function getName(){
+	var year=parseInt(document.getElementById("year").value);
+	var month =parseInt(document.getElementById("month").value);
+	var day=parseInt(document.getElementById("day").value);
+	var male = document.getElementById("male");
+	var female = document.getElementById("female");
+	
+	//validation
+	if(day<=0||day>31)
+	alert("Oops!please enter a valid day");
+	else if(month<0||month>12 ||month ==2 && day>29)
+	alert("Oops!please enter a valid month");
+	
+	
+	var day = new Date(year + "/" + month + "/" + day);
+	
+	var birthDay=day.getDay();
+	if(male.checked==true){
+	   alert("You were born on " +daysOfWeek[birthDay]+ " and your akan name is " + maleAkanNames[birthDay])
+	}
+	else if(female.checked==true){
+	   alert("You were born on " +daysOfWeek[birthDay]+ " and your akan name is " + femaleAkanNames[birthDay])
+
+	}
+
+	}
+	
+	

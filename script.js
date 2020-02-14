a5ab11e1c06df1291b6f7a4a2574bb4a250c3917
BA <engineervinceblair@gmail.com>
2020-02-14 13:21:04 +0300

    created variables for female and male names and

---
+++ script.js
@@ -0,0 +1,22 @@
+var femaleAkanNames = [
+	"Akosua, <img src='https://bit.ly/2JsWQ5t' height='200px'>",
+	"Adwoa, <img src='https://bit.ly/369OsS7' height='200px'>",
+	"Abenaa, <img src='https://bit.ly/2MRihiN' height='200px'>",
+	"Akua, <img src='https://bit.ly/2Poskxu' height='200px'>",
+	"Yaa, <img src='https://bit.ly/2PoBN7T' height='200px'>",
+	"Afua, <img src='https://bit.ly/2pj47xU' height='200px'>",
+	"Ama, <img src='https://bit.ly/348GTcI' height='200px'>"
+];
+var maleAkanNames = [
+	"Kwasi, <img src='https://bit.ly/2omOfdc' height='200px'>",
+	"Kwadwo, <img src='https://bit.ly/31SWwmV' height='200px'>",
+	"Kwabena, <img src='https://bit.ly/2PphxDi' height='200px'>",
+	"Kwaku, <img src='https://bit.ly/3683NCG' height='200px'>",
+	"Yaw, <img src='https://bit.ly/36eVwx0' height='200px'>",
+	"Kofi, <img src='https://bit.ly/2JsXMH1' height='200px'>",
+	"Kwame, <img src='https://bit.ly/2BRxDNG' height='200px'>"
+];
+var daysWeek=[
+    "sunday"
+    mo
+]
